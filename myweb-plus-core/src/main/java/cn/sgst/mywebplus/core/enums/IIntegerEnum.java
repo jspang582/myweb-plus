f8a7bb77600cf090991e85da80898b61c2480a3b
fli <fli@sstir.cn>
2021-03-11 23:27:51 +0800

    枚举类扩展和优化

---
+++ myweb-plus-core/src/main/java/cn/sgst/mywebplus/core/enums/IIntegerEnum.java
@@ -0,0 +1,12 @@
+package cn.sgst.mywebplus.core.enums;
+
+/**
+ * K:Integer
+ * V:String
+ *
+ * @author: fli
+ * @email: fli@sstir.cn
+ * @date: 2021/3/11 22:23
+ */
+public interface IIntegerEnum extends IEnum<Integer,String> {
+}
